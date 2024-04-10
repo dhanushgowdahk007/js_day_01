ce0a89eb279af4a07bcb5e06baadb8cfe5f53520
dhanushgowdahk007 <dhanushgowdahk04@gmail.com>
2024-04-10 18:16:50 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,27 @@
+const product = {
+    title: "Ball pen",
+    rating: 4,
+    offer: 5,
+    color: "Black",
+    price: 275,
+    isDeal: true
+};
+
+console.log(product);
+console.log(product.color);
+console.log(product.title);
+console.log(product.rating);
+console.log(typeof(product));
+console.log(typeof(product.offer));
+
+const profile = {
+    fullName: "Shrada Khapra",
+    isFollow: true,
+    posts: 195,
+    following: 4,
+    bio: "To educate someone is the highest privilege",
+    job: "Enterpreneur"
+};
+
+console.log(profile);
+console.log(typeof(profile.isFollow));
